6e925b8314b4ebf3ec1e66ce84479985df19a4c6
Zhengyi <zhengyiluo5@gmail.com>
2024-07-21 18:09:59 -0400

    data

--- download_data.sh
+++ download_data.sh
@@ -4,6 +4,27 @@ gdown https://drive.google.com/uc?id=1bLp4SNIZROMB7Sxgt0Mh4-4BLOPGV9_U -O  sampl
 gdown https://drive.google.com/uc?id=1arpCsue3Knqttj75Nt9Mwo32TKC4TYDx -O  sample_data/ # all shapes from AMASS
 gdown https://drive.google.com/uc?id=1fFauJE0W0nJfihUvjViq9OzmFfHo_rq0 -O  sample_data/ # sample standing neutral data.
 gdown https://drive.google.com/uc?id=1uzFkT2s_zVdnAohPWHOLFcyRDq372Fmc -O  sample_data/ # amass_occlusion_v3
-gdown https://drive.google.com/uc?id=1BDUJ3nlub9tv1fF0UMANVUryJo1h5-lo -O  sample_data/ # amass_isaac_simple_run_upright_slim
 gdown https://drive.google.com/uc?id=1ztyljPCzeRwQEJqtlME90gZwMXLhGTOQ -O  output/HumanoidIm/pulse_vae/
+gdown https://drive.google.com/uc?id=1JbK9Vzo1bEY8Pig6D92yAUv8l-1rKWo3 -O  output/HumanoidIm/phc_comp_3/
+gdown https://drive.google.com/uc?id=1pS1bRUbKFDp6o6ZJ9XSFaBlXv6_PrhNc -O  output/HumanoidIm/phc_3/
 gdown https://drive.google.com/uc?id=1S7_9LesLjfsFYqi4Ps6Sjzyuyun0Oaxi -O  output/HumanoidIm/pulse_vae_x/
+
+
+###### Download sample video data
+gdown https://drive.google.com/uc?id=1BDUJ3nlub9tv1fF0UMANVUryJo1h5-lo -O  sample_data/ # amass_isaac_simple_run_upright_slim
+gdown https://drive.google.com/uc?id=1XvwTSeOb_0qZo9ADAp4ftiLrvm6JHJ4h -O  sample_data/ # Table Tennis
+gdown https://drive.google.com/uc?id=1lgzAup2qotweWDMxY9TygbRTea4jUZb0 -O  sample_data/ # Boxing
+gdown https://drive.google.com/uc?id=1fbieKIv6dFtv5Xr_0P6f_eXdnu-mhVpl -O  sample_data/ # Soccer
+gdown https://drive.google.com/uc?id=1QKGGXMWjooNC4iJj0LWGC6iDd_cMP790 -O  sample_data/ # javelin_state
+gdown https://drive.google.com/uc?id=1ospAiML-R8nSDZJCGUfDpN0ghTSCQ0Qu -O  sample_data/ # javelin data
+gdown https://drive.google.com/uc?id=1M2IeDiPRnGUc9dMrUdm_FVClU5MptA2Q -O  sample_data/ # golf
+gdown https://drive.google.com/uc?id=1IN1zerTkEDn0RoeKtGn-2s3mxhJixUQl -O  sample_data/ # fencing
+gdown https://drive.google.com/uc?id=1d_cYa4i69Y_F8-6QoaC6vFWGON4q7ML3 -O  sample_data/ # freethrows states
+gdown https://drive.google.com/uc?id=1TL6uGa_Yjwche7cdpdQr-Ml9211_0rap -O  sample_data/ # basketball states
+gdown https://drive.google.com/uc?id=1vUb7-j_UQRGMyqC_uY0YIdy6May297K5 -O  sample_data/ # standing amass x
+
+
+
+
+
+
