ef4fc6ad2d9db4da53e3820dd298b7ddba3af3f1
Zhengyi <zhengyiluo5@gmail.com>
2024-08-30 14:23:38 -0400

    some naming issues

--- scripts/run_boxing.sh
+++ scripts/run_boxing.sh
@@ -2,7 +2,7 @@ export OMP_NUM_THREADS=1
 python phc/run_hydra.py     project_name=SMPLOlympics num_agents=2     \
         learning=amp_z_self_play exp_name=boxing_pulse \
         env=env_amp_z env.num_envs=2048 env.task=HumanoidBoxingZ env.enableTaskObs=True  \
-        env.stateInit=Start    robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae/Humanoid.pth"]      \
+        env.stateInit=Start    robot=smpl_humanoid_boxing +env.models=["output/HumanoidIm/pulse_vae_iclr/Humanoid.pth"]      \
         env.motion_file=./sample_data/video_boxing_afterproc_upright.pkl headless=True env.episode_length=300  learning.params.config.switch_frequency=250
 
 
--- scripts/run_fencing.sh
+++ scripts/run_fencing.sh
@@ -2,7 +2,7 @@ export OMP_NUM_THREADS=1
 python phc/run_hydra.py     project_name=SMPLOlympics num_agents=2     \
         learning=amp_z_self_play exp_name=fencing_pulse \
         env=env_amp_z env.num_envs=2048 env.task=HumanoidFencingZ env.enableTaskObs=True  \
-        env.stateInit=Start    robot=smpl_humanoid_fencing +env.models=["output/HumanoidIm/pulse_vae/Humanoid.pth"]      \
+        env.stateInit=Start    robot=smpl_humanoid_fencing +env.models=["output/HumanoidIm/pulse_vae_iclr/Humanoid.pth"]      \
         env.motion_file=./sample_data/fencing_all.pkl headless=True env.episode_length=300  learning.params.config.switch_frequency=250
 
 
