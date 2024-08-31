913598f7f724fbd81c20fb8aebef9b33196d9277
Zhengyi "Zen" Luo <zhengyiluo5@gmail.com>
2024-08-30 21:00:54 -0700

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 mkdir sample_data
-mkdir -p output output/HumanoidIm/ output/HumanoidIm/phc_3 output/HumanoidIm/phc_comp_3 output/HumanoidIm/pulse_vae output/HumanoidIm/pulse_vae_x/
+mkdir -p output output/HumanoidIm/ output/HumanoidIm/phc_3 output/HumanoidIm/phc_comp_3 output/HumanoidIm/pulse_vae_iclr output/HumanoidIm/pulse_vae_x/
 gdown https://drive.google.com/uc?id=1bLp4SNIZROMB7Sxgt0Mh4-4BLOPGV9_U -O  sample_data/ # filtered shapes from AMASS
 gdown https://drive.google.com/uc?id=1arpCsue3Knqttj75Nt9Mwo32TKC4TYDx -O  sample_data/ # all shapes from AMASS
 gdown https://drive.google.com/uc?id=1fFauJE0W0nJfihUvjViq9OzmFfHo_rq0 -O  sample_data/ # sample standing neutral data.
